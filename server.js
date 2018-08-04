1a098105cdc98d6f18a929aabd9de7105d01ef42
umair210 <40460806+umair210@users.noreply.github.com>
2018-08-04 10:41:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,60 @@ var db = mongoose.connection;
 var bodyParser=require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/public')); //using to get folders of public this is a root
+
 var URL=('mongodb://localhost:27017/loginform');
 const Users = require('./models/models');
+const Logins=require('./models/loginschema');
+var jwt=require('jsonwebtoken');
+var supersecret='iamumairkhan';
+var adminauth=express.Router();
+adminauth.get('/',function (req,res) {
+    res.sendFile(path.join(__dirname + '/indexx.html'))
+});
+app.post('/authentication',function (req,res) {
+mongoose.connect('mongodb://localhost:27017/signup',function (err,db) {
+    if(err) throw err;
+     Logins.findOne({email:req.body.email}).exec(function (err,user) {
+        if (err) throw err;
+        if(!user){
+            res.json('User not foound')
+        }
+        else if(user)
+        {
+            var validpassword=Logins.comparePassword(req.body.password);
+            if(!validpassword)
+            {res.send('password not valid')}
+            else if(validpassword)
+            {
+                res.send('password correct');
+            }
+        }
+     });
+})  ;
+
+
+});
+//=======Signup or registration======
+var registration=express.Router();
+registration.get('/',function (req,res) {
+    res.sendFile(path.join(__dirname + '/register.html' ))
+});
+app.post('/signup',function (req,res,next) {
+   mongoose.connect('mongodb://localhost:27017/signup',function (err,db) {
+       if (err) throw err;
+       var Signup=new Logins(req.body);
+        Signup.firstname=req.body.first_name;
+        Signup.secondname=req.body.second_name;
+        Signup.email=req.body.email;
+        Signup.password=req.body.password;
+        Signup.confirmpassword=req.body.conf_password;
+       Signup.save();
+       res.json('hello signup');
+  console.log(Signup);
+   }) ;
+});
 //======get the login page======
 var adminroute=express.Router();
 adminroute.get('/',function (req,res) {
@@ -51,29 +103,27 @@ app.get('/get',function (req,res) {
             loginformfill.push(doc);    //pushing into array one by one
             },  function() {
             db.close();
-            res.json(loginformfill);    //Showing the data saved in array in JSON format
+            if(loginformfill=== undefined || loginformfill.length==0 )
+            {
+            res.json('No data is present in the db');
+            }
+            else {
+                res.json(loginformfill); //Showing the data saved in array in JSON format
+            }
             }); // STORING DATA IN AN ARRAY USING LOOP AND SHOWING IT IN 'JSON' FORMAT
         });
     });
-//=======adding data in mongodb======
+//============ADD================
 app.post('/addname',function (req,res) {
-    //var savedatafile=[];
-    //MongoClient.connect('mongodb://localhost:27017/savedata', function (err, db) {
     mongoose.connect('mongodb://localhost:27017/savedata');
         console.log('hello im here');
-       // if (err) throw err;
-
         var mydata = new Users(req.body);
         console.log(mydata);
-     //   var mydb=db.db('savedata');
-       // mydata.forEach(function (doc,err) {
-         //  if (err) throw err;
              mydata.name = req.body.name;
              mydata.email = req.body.email;
              mydata.age = req.body.age;
            mydata.save();
             res.json('hello task completed');
-
 });
 //==========UPDATE===========
 app.post('/update',function (req, res) {
@@ -103,6 +153,8 @@ db1.collection('logins').removeOne({_id:objectId(req.body.id)},function (err,res
 });
     });
 });
-app.use('/login',adminroute);
-app.listen(8080);
+app.use('/login',adminroute); //open Login form for crud
+app.use('/register',registration); //open signup form
+app.use('/auth',adminauth);//authentication
+app.listen(8000);
 console.log('server is going to start');
